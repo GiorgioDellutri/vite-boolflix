ba1528a9dafa352efa11e9334d65a2a0d121369a
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-18 00:04:03 +0100

    moved search bar and button to AppHeader + comments

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,11 +7,9 @@ export default {
     data() {
         return {
             store,
-
         }
     },
     methods: {
-
     }
 }
 </script>
@@ -21,6 +19,21 @@ export default {
         <div class="row">
             <div class="col-12">
                 <h1 class="text-danger">BoolFlix</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-3">
+                <div class="input-group mb-3">
+                    <!-- utilizzo v-model per creare una relazione tra l'input e la proprietà searchMovie di store.js,
+                    quindi quando si digita qualcosa nell'input si aggiorna automaticamente il valore di searchMovie in
+                    store.js.Quando si preme enter o si clicca sul bottone search si emette un evento chiamato 'search' con argomento store.searchMovie. Questo evento è utilizzato in App.vue per effettuare la ricerca -->
+                    <input type="text" class="form-control" v-model="store.searchMovie"
+                        @keyup.enter="$emit('search', store.searchMovie)" placeholder="Search movies or tv show"
+                        aria-label="Search movies or tv show" aria-describedby="button-addon2">
+                    <button @click="$emit('search', store.searchMovie)"
+                        class="btn btn-outline-secondary btn-danger text-dark fw-5 " type="button"
+                        id="button-addon2">Search</button>
+                </div>
             </div>
         </div>
     </div>
